53a8c79050b70acfc5e2afe8aa9fa41f35f8168b
SamuelJTaylor22 <sjtaylor1998@gmail.com>
2020-09-30 11:12:26 -0600

    authConfig added

--- clientsrc/src/authConfig.js
+++ clientsrc/src/authConfig.js
@@ -1,3 +1,3 @@
-export const domain = "";
-export const audience = "";
-export const clientId = "";
+export const domain = "deciderapp.us.auth0.com";
+export const audience = "https://deciderapp";
+export const clientId = "3tHh1A0HRx9ZMwTQVOrmRS92ejVj9AIA";
