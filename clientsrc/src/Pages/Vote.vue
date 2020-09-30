52f3416035744de66af2b569a012ba559a97e2fc
Lawrence Haney <Lawrence.r.haney@gmail.com>
2020-09-30 12:54:08 -0600

    staring vote mock

---
+++ clientsrc/src/Pages/Vote.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-2"><i class="fa fa-arrow-circle-o-up" aria-hidden="true"></i></div>
+      <div class="col-10 card"><img src="https://cdn.cloudflare.steamstatic.com/steam/apps/945360/header.jpg?t=1598556351" alt=""></div>
+      <div class="col-2"><i class="fa fa-arrow-circle-o-down" aria-hidden="true"></i></div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
