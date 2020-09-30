fcc4578059a2cf4bf6334bb759b05aeca6fc51e1
Ben W <bwiedeback@gmail.com>
2020-09-30 14:42:52 -0600

    rooms template exists

--- clientsrc/src/Pages/Rooms.vue
+++ clientsrc/src/Pages/Rooms.vue
@@ -1,5 +1,52 @@
 <template>
-  
+  <div class="container-fluid">
+    <div style="height: 10vh"></div>
+    <div class="row">
+      <div class="offset-2 col-8">
+        <div class="jumbotron" style="height: 40vh">
+          <h1 class="display-4">Hello, world!</h1>
+          <p class="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
+          <hr class="my-4">
+          <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
+        </div>
+      </div>
+    </div>
+    <div style="height: 10vh"></div>
+    <div class="row">
+      <div class="offset-1 col-10">
+        <div class="card text-center" style="height: 60vh">
+          <div class="card-header">
+            Create a New Poll
+          </div>
+          <div class="card-body">
+            <h5 class="card-title">Special title treatment</h5>
+            <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+          </div>
+          <div class="card-footer text-muted">
+            2 days ago
+          </div>
+        </div>
+      </div>
+    </div>
+    <div style="height: 10vh"></div>
+    <div class="row">
+      <div class="offset-1 col-10">
+        <div class="card text-center" style="height: 60vh">
+          <div class="card-header">
+            My Previous Polls
+          </div>
+          <div class="card-body">
+            <h5 class="card-title">Special title treatment</h5>
+            <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+          </div>
+          <div class="card-footer text-muted">
+            2 days ago
+          </div>
+        </div>
+      </div>
+    </div>
+    <div style="height: 10vh"></div>
+  </div>
 </template>
 
 <script>
