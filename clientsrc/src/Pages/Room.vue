3bd6afbf0088452ad55dc36ef716cfeee4ff3238
Lawrence Haney <Lawrence.r.haney@gmail.com>
2020-09-30 14:15:59 -0600

    starting room

--- clientsrc/src/Pages/Room.vue
+++ clientsrc/src/Pages/Room.vue
@@ -1,4 +1,15 @@
 <template>
+<div class="fluid-container">
+  <div class="row justify-content-center">
+    <div class="card bg-primary">
+      <form class="row justify-content-center">
+          <input type="text" placeholder="title" required class="col-10 m-1" />
+          <textarea type="text" placeholder="description" class="col-10 m-1" />
+          <button type="submit" class="btn btn-outline-light m-1">New poll</button>
+    </form>
+    </div>
+  </div>
+</div>
   
 </template>
 
