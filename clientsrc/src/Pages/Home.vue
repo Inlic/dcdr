306db502e163ea242c3949f1ee46e506f02ad755
Ben W <bwiedeback@gmail.com>
2020-09-30 16:27:35 -0600

    styling changes

--- clientsrc/src/Pages/Home.vue
+++ clientsrc/src/Pages/Home.vue
@@ -22,7 +22,7 @@
           </div>
           <div class="card-body">
             <h5 class="card-title">Special title treatment</h5>
-            <img class="card-img" src="https://i.imgur.com/QT4H77w.jpg" alt="Card image" style="height: 60vh">
+            <img class="card-img" src="https://i.imgur.com/QT4H77w.jpg" alt="Card image" style="height: 30vh">
             <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
           </div>
           <div class="card-footer text-muted">
