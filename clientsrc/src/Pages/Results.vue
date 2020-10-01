fffcf05b8222967d71ab22d7e5618dc61c2265b0
Ben W <bwiedeback@gmail.com>
2020-10-01 15:24:27 -0600

    styling changes

--- clientsrc/src/Pages/Results.vue
+++ clientsrc/src/Pages/Results.vue
@@ -10,8 +10,10 @@
     </div>
     <div style="height: 2vh"></div>
     <div class="row">
-      <div class="col-12 text-center">
+      <div class="offset-4 col-4 text-center">
+        <div class="card">
           <img class="img-fluid" src="https://images-na.ssl-images-amazon.com/images/I/416zAtw+MML._SX306_BO1,204,203,200_.jpg">
+          </div>
       </div>
     </div>
     <div style="height: 5vh"></div>
