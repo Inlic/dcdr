99b7701ac42415e71c48868a128dfb31b174b5d1
CoreyWhitmore <cjwhit97@hotmail.com>
2020-10-01 09:43:24 -0600

    Room.vue sidescroll fixed

--- clientsrc/src/Pages/Room.vue
+++ clientsrc/src/Pages/Room.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="fluid-container">
-    <div class="row justify-content-center">
+    <div class="d-flex justify-content-center">
       <div class="card bg-primary text-center">
         <h1>{{room.name}}</h1>
         <h4>{{room.code}}</h4>
-        <form @submit.prevent="createGame" class="row justify-content-center">
+        <form @submit.prevent="createGame" class="justify-content-center">
           <input type="text" placeholder="name" required v-model="newGame.name" class="col-10 m-1" />
           <input type="Url" placeholder="image" v-model="newGame.imgUrl" class="col-10 m-1" />
           <button type="submit" class="btn btn-outline-light m-1">Add a game</button>
@@ -17,33 +17,33 @@
 </template>
 
 <script>
-export default {
-  name: "Room",
-  data(){
-    return{
-      newGame: {}
-    }
-  },
-  mounted(){
-    this.$store.dispatch("getRoomByCode", this.$route.params.code)
-    this.$store.dispatch('joinRoom', "room")
-  },
-  computed: {
-    room(){
-      return this.$store.state.room
-    }
-  },
-  methods:{
-    createGame(){
-      this.newGame.roomId = this.room.id
-      this.$store.dispatch("createGame", this.newGame)
+  export default {
+    name: "Room",
+    data() {
+      return {
+        newGame: {}
+      }
     },
-    startPoll(){
-      this.$store.dispatch("getGames", this.room.id)
-      this.$router.push({name:'Vote', params:{code:this.room.code}})
+    mounted() {
+      this.$store.dispatch("getRoomByCode", this.$route.params.code)
+      this.$store.dispatch('joinRoom', "room")
     },
+    computed: {
+      room() {
+        return this.$store.state.room
+      }
+    },
+    methods: {
+      createGame() {
+        this.newGame.roomId = this.room.id
+        this.$store.dispatch("createGame", this.newGame)
+      },
+      startPoll() {
+        this.$store.dispatch("getGames", this.room.id)
+        this.$router.push({ name: 'Vote', params: { code: this.room.code } })
+      },
+    }
   }
-}
 </script>
 
 <style>
