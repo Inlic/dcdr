520a0793ff7194d8d122657a3092facda2798ac5
CoreyWhitmore <cjwhit97@hotmail.com>
2020-10-02 11:13:44 -0600

    styling changes for home page

--- clientsrc/src/Pages/Home.vue
+++ clientsrc/src/Pages/Home.vue
@@ -1,50 +1,56 @@
 <template>
   <div class="background container-fluid">
-    <div style="height: 10vh"></div>
-    <div class="row">
+    <div class="row mt-5">
       <div class="col-8">
-          <img class="card-img" src="@/assets/neontogether.jpg" alt="Card image" style="height: 60vh; border-radius: 10px; filter: contrast(135%);">
+        <img class="card-img" src="@/assets/neontogether.jpg" alt="Card image"
+          style="height: 60vh; border-radius: 10px; filter: contrast(135%);">
       </div>
       <div class="col-4">
         <div class="card text-center border-0" style="height: 60vh">
-          <div class="card-header bg-dark flashy neon red" style="font-size: 2em">
-            Level Up Your Gaming Experience
+          <div class="card-header bg-dark flashy neon red">
+            <h3>Level Up Your Gaming Experience</h3>
           </div>
           <div class="card-body bg-dark">
-            <p class="neon blue" style="font-size: 3.2em">Don't Waste Time Deciding what to play.Match with your friends, find a game you all love, and get to the game faster.</p>
+            <p class="neon blue">Don't Waste Time Deciding what to play.Match with your
+              friends, find a game you all love, and get to the game faster.</p>
           </div>
         </div>
       </div>
     </div>
-    <div style="height: 10vh"></div>
-    <div class="row">
+    <div class="row mt-5">
       <div class="col-5">
         <div class="card text-center border-0" style="height: 60vh">
-            <div class="card-header bg-dark flashy neon red" style="font-size: 2em">
-              Persistent Polls
-            </div>
-            <div class="card-body bg-dark neon blue">
-            <p style="font-size: 2.5em">Love the games you voted on last time, but want to create another poll?  No problem your prior polls are saved to your account.</p>
-            </div>
+          <div class="card-header bg-dark flashy neon red" style="font-size: 2em">
+            Persistent Polls
           </div>
+          <div class="card-body bg-dark neon blue">
+            <p>Love the games you voted on last time, but want to create another poll? No
+              problem your prior polls are saved to your account.</p>
+          </div>
+        </div>
       </div>
       <div class="col-7">
-        <img class="card-img" src="@/assets/gameon.jpg" alt="Card image" style="height: 60vh; filter: brightness(75%); filter: contrast(150%)">
-      </div>   
+        <img class="card-img" src="@/assets/gameon.jpg" alt="Card image"
+          style="height: 60vh; filter: brightness(75%); filter: contrast(150%)">
+      </div>
     </div>
     <div style="height: 10vh"></div>
     <div class="row">
       <div class="col-3">
-        <img class="img-fluid" src="@/assets/neondesktop.jpg" style="height: 60vh; width: 100%; border-radius: 10px; filter: brightness(85%); filter: contrast(110%);">
+        <img class="img-fluid" src="@/assets/neondesktop.jpg"
+          style="height: 60vh; width: 100%; border-radius: 10px; filter: brightness(85%); filter: contrast(110%);">
       </div>
       <div class="col-3">
-        <img class="img-fluid" src="@/assets/racinggirl.jpg" style="height: 60vh; width: 100%; border-radius: 10px; filter: brightness(85%); filter: contrast(110%);">
+        <img class="img-fluid" src="@/assets/racinggirl.jpg"
+          style="height: 60vh; width: 100%; border-radius: 10px; filter: brightness(85%); filter: contrast(110%);">
       </div>
       <div class="col-3">
-        <img class="img-fluid" src="@/assets/neoncompguts.jpg" style="height: 60vh; width: 100%; border-radius: 10px; filter: brightness(85%); filter: contrast(110%);">
+        <img class="img-fluid" src="@/assets/neoncompguts.jpg"
+          style="height: 60vh; width: 100%; border-radius: 10px; filter: brightness(85%); filter: contrast(110%);">
       </div>
       <div class="col-3">
-        <img class="img-fluid" src="@/assets/vrdude.jpg" style="height: 60vh; width: 100%; border-radius: 10px; filter: brightness(85%); filter: contrast(110%);">
+        <img class="img-fluid" src="@/assets/vrdude.jpg"
+          style="height: 60vh; width: 100%; border-radius: 10px; filter: brightness(85%); filter: contrast(110%);">
       </div>
     </div>
     <div style="height: 10vh"></div>
@@ -52,10 +58,10 @@
 </template>
 
 <script>
-export default {
-  name: "Home",
-  mounted(){
-    
-  },
-};
+  export default {
+    name: "Home",
+    mounted() {
+
+    },
+  };
 </script>
