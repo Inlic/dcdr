34ddc27be99cb7b5f6e1a873df82bdb9e9970722
SamuelJTaylor22 <sjtaylor1998@gmail.com>
2020-10-05 14:27:29 -0600

    added veto'd indicator

--- clientsrc/src/components/GameComponent.vue
+++ clientsrc/src/components/GameComponent.vue
@@ -2,7 +2,9 @@
   <div class="card bg-dark">
     <h1 class="flashy neon orange">{{gameData.name}}</h1>
     <img class="img-fluid" :src="gameData.imgUrl">
-    <h6 class="flashy neon orange">Score: {{gameData.score}}</h6>
+    <h6 v-if="!gameData.veto" class="flashy neon orange">Score: {{gameData.score}}</h6>
+    <h6 v-else class="flashy neon purple">V-<i class="fas fa-frog"></i></h6>
+
   </div>
 </template>
 
