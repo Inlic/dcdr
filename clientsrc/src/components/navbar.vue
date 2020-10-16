1f0a54a1e543ba2bc674c8ec60800e81578d096e
SamuelJTaylor22 <sjtaylor1998@gmail.com>
2020-10-16 13:33:19 -0600

    fixed being able to hit escape to join a room

--- clientsrc/src/components/navbar.vue
+++ clientsrc/src/components/navbar.vue
@@ -90,9 +90,14 @@
           let res = await as.addName()
           this.$store.dispatch("setMyName", res.value)
         }
+        if(this.$store.state.name){
         this.$store.dispatch('joinRoom', `${this.code}`)
         this.$store.dispatch("getGames", this.code)
         this.$router.push({ name: 'Room', params: { code: this.code } })
+        }
+        else{
+          as.nameNotice()
+        }
       },
     },
     components:{
