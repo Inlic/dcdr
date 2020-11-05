6e4fe2f306dccab36063db4995c62a46ba3d3c10
Ben Wiedeback <66647604+Inlic@users.noreply.github.com>
2020-11-05 16:15:30 -0700

    update auth settings for fork

--- clientsrc/src/authConfig.js
+++ clientsrc/src/authConfig.js
@@ -1,3 +1,3 @@
-export const domain = "deciderapp.us.auth0.com";
-export const audience = "https://deciderapp";
-export const clientId = "3tHh1A0HRx9ZMwTQVOrmRS92ejVj9AIA";
+export const domain = "forestlogin223.us.auth0.com";
+export const audience = "https://AuthCheckApi.com";
+export const clientId = "t9ZTG6p58iyE5ncgtnpMQv65PT3QKsQE";
